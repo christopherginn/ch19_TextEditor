b6daff52a0141e00cc4ae0b37e31c92d9e7f8bdb
christopherginn <hey.im.chris@live.com>
2022-07-10 11:32:46 -0400

    fixed service worker issue

--- Main/client/webpack.config.js
+++ Main/client/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = () => {
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './/index.html',
+        template: './index.html',
         title: 'Another Text Editor'
       }),
       new InjectManifest({
